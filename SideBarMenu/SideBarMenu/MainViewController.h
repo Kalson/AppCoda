fdeecdcadbea92fad7804df4458c27cd5c2912e8
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-06-07 16:35:09 -0700

    Created the Front & Rear Controllers and toggle system - SideBarMenu

---
+++ SideBarMenu/SideBarMenu/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SideBarMenu
+//
+//  Created by KaL on 6/7/15.
+//  Copyright (c) 2015 KaL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ SideBarMenu/SideBarMenu/MainViewController.m
@@ -0,0 +1,54 @@
+//
+//  MainViewController.m
+//  SideBarMenu
+//
+//  Created by KaL on 6/7/15.
+//  Copyright (c) 2015 KaL. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "SWRevealViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+    // retrive the reveal View Controller
+    SWRevealViewController *revealVC = self.revealViewController;
+    
+    // create the sidebar button
+    UIBarButtonItem *sideBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:revealVC action:@selector(revealToggle:)];
+    self.navigationItem.leftBarButtonItem = sideBarButton;
+    
+    if (revealVC) {
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    }
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
